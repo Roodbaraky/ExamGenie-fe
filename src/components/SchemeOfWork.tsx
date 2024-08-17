cb332f9b1cd1fc617feeed7597cae441bec2c002
roodbaraky <kooroshr@hotmail.com>
2024-08-17 12:08:52 +0100

    Added scroll to SoW

--- src/components/SchemeOfWork.tsx
+++ src/components/SchemeOfWork.tsx
@@ -9,23 +9,26 @@ interface SchemeOfWorkProps {
 export default function SchemeOfWork({ weeks }: SchemeOfWorkProps) {
   weeks.sort((a, b) => a.week_number - b.week_number);
   return (
-    <div className="flex flex-col mx-36">
+    <div className="flex flex-col mx-36 ">
       <h2 className="text-2xl">Scheme of Work</h2>
-      {weeks.map((week: Week) => (
-        <div key={week.week_number} className="flex gap-4 p-1">
-          <div>Week {week.week_number}:</div>
-          {[
-            week.tags.map((tag) => (
-              <a key={tag} className="btn">
-                {tag.replace("-", ": ")}
-              </a>
-            )),
-            <a key={"plus-btn"} className="btn btn-outline">
-              +
-            </a>,
-          ]}
-        </div>
-      ))}
+
+      <div className="overflow-scroll max-h-96">
+        {weeks.map((week: Week) => (
+          <div key={week.week_number} className="flex gap-4 p-1">
+            <div>Week {week.week_number}:</div>
+            {[
+              week.tags.map((tag) => (
+                <a key={tag} className="btn">
+                  {tag.replace("-", ": ")}
+                </a>
+              )),
+              <a key={"plus-btn"} className="btn btn-outline">
+                +
+              </a>,
+            ]}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
